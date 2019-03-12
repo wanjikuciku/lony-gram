bb89b5b8f8fe179d19497731e5e55f5f0dc69301
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-12 10:33:06 +0300

    add functionality

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render,redirect
-from .models import Image,Follow,Profile,Comments,idss
+from .models import Image,Follow,Profile,Comments
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
@@ -34,40 +34,6 @@ def index(request):
         else:
             image.user_has_liked = False
     return render(request,"index.html", {"images":images,"user":request.user})
-
-def like(request):
-    user = request.user
-    if request.method == 'POST':
-        ObjectId = int(request.POST['objectid'])
-        Tip = str(request.POST['contentType'])
-
-        likes = LikeModel.objects.filter(object_id=ObjectId, content_object=Tip) # in here we filtered the particular post with its id
-        if likes: # if the particular post is there
-            if str(user) in str(likes): # then we check the user which is us, in there
-                like_obj = LikeModel.objects.get(user=user,object_id=ObjectId, content_object=Tip) #if we there and we returned this data, this part for saving data, I mean if this data is already created than we dont have to delete and create again, we just change LikeModel.liked true or false state, so that if you create like and it will never delete, it just change liked or like state
-            else:
-                pass
-
-        if Tip == 'UserPost':
-            post_content_type_by = UserPost.objects.all().first()
-
-            if str(user) not in str(likes):
-                like = LikeModel.objects.create(user=user, liked=True, content_object=ContentType.objects.get_for_model(Tip), object_id=ObjectId)
-                like.save() # if data is created then we say 'new'
-                okey = 'new'
-
-            elif str(user) in str(likes) and like_obj.liked:
-                like_obj.liked = False
-                like_obj.save() # if data is already there, then we save it False
-                okey = 'false'
-
-            elif str(user) in str(likes) and like_obj.liked == False:
-                like_obj.liked = True
-                like_obj.save() # if data is already changed to False and we save again to True
-                okey = 'true'
-
-
-    return render(request,'ajaxlike.html',{'likes':likes,'okey':okey})
 
 def like(request):
     user = request.user
